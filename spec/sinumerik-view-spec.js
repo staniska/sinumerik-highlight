78d6a6c8c01459fe5b4676eef7193567862e1d1b
a3boot <a3boot.babaev@gmail.com>
2020-02-28 10:46:44 +0300

    Sinumerik highlight and some snippets. Right panel not working yet.

---
+++ spec/sinumerik-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import SinumerikView from '../lib/sinumerik-view';
+
+describe('SinumerikView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
